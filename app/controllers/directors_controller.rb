c65e37d4e5120c424b4465a54ec41725d74a3783
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2022-04-29 14:19:09 +0800

    created new and create actions in controllers, and view with form for new

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,4 +6,24 @@ class DirectorsController < ApplicationController
   def show
     @director = Director.find(params[:id])
   end
+
+  def new
+    @director = Director.new # needed to instantiate the form_for
+  end
+
+  def create
+    @director = Director.new(director_params)
+    if @director.save
+      flash[:notice] = 'Director has been successfully created.'
+      redirect_to directors_path(@director)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def director_params
+    params.require(:director).permit(:name)
+  end
 end
--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -6,4 +6,24 @@ class DocsController < ApplicationController
   def show
     @doc = Doc.find(params[:id])
   end
+
+  def new
+    @doc = Doc.new # needed to instantiate the form_for
+  end
+
+  def create
+    @doc = Doc.new(doc_params)
+    if @doc.save
+      flash[:notice] = 'Doc has been successfully created.'
+      redirect_to docs_path(@doc)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def doc_params
+    params.require(:doc).permit(:name)
+  end
 end
