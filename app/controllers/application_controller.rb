5c974303a94c541fc4dc65559915d624acd09f0d
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2022-05-13 00:50:03 +0800

    corrected bug on home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  # protect_from_forgery
+
+  # helper_method :current_user, :signed_in?, :is_admin?
+
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
+
+  # def signed_in?
+  #   !!current_user
+  # end
+
+  # def is_admin?
+  #   signed_in? ? current_user.admin : false
+  # end
+
 end
