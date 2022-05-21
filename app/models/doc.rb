0141848b4b20693bf371a755b635a7de22195e31
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2022-05-21 18:44:52 +0800

    validate presence and uniqueness of name

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,6 +1,7 @@
 class Doc < ApplicationRecord
   include PgSearch::Model
   multisearchable against: [:name]
+
   has_many :projects
   has_many :directors, through: :projects
   has_many :awards, inverse_of: :doc, dependent: :destroy
@@ -8,6 +9,6 @@ class Doc < ApplicationRecord
   has_one_attached :poster
   accepts_nested_attributes_for :awards, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
+
+  validates :name, presence: true, uniqueness: true
 end
-
-
